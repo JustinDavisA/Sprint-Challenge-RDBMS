8155360d2d2c383fcabea44143a209f0065b60f5
justindavisa <justindavisa@gmail.com>
2019-03-29 11:07:42 -0400

    dbConfig, index, knex added and setup, awaiting endpoint

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const db = require('./data/dbConfig.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
